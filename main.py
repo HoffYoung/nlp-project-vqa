df7884df8fa66814ad551bea05d5e0c997e235f7
Yang Haofeng <1183045207@qq.com>
2022-05-27 13:07:22 +0800

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+# 数据集路径
+basic_path = "./data/"
+# opencv 人脸检测模型在数据集 mindspore_model_data/opencv_dnn 文件夹中
+images_test_path = basic_path + 'images/test'
+images_train_path = basic_path + 'images/train'
+images_val_path = basic_path + 'images/val'
+print(opencv_dnn_path)
+# 查看文件夹里面文件
+os.listdir(opencv_dnn_path)
---
+++ preprocess/preprocess.py
@@ -0,0 +1,31 @@
+import os
+import json
+
+data_path = '../data/annotations/tran.json'
+
+data = json.load(data_path)
+
+
+
+
+
+# read original data
+
+
+
+
+
+
+
+
+# create MindRecord
+
+
+
+
+
+
+
+
+
+# load data from MindRecord
